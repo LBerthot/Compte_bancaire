95a36cec256f1f20f199d737908adfae805ad539
Ludivine <hadesvapeur8@gmail.com>
2021-04-10 23:14:12 +0200

    Changes of tests

--- src/fr/afpa21013/tests/TestLudivine.java
+++ src/fr/afpa21013/tests/TestLudivine.java
@@ -1,21 +1,11 @@
 package fr.afpa21013.tests;
 
-import fr.afpa21013.BankAccount;
-import fr.afpa21013.Client;
-import fr.afpa21013.service.BankAccountService;
-import fr.afpa21013.service.ClientService;
-
 public class TestLudivine {
 
 	public static void main(String[] args) {
-		ClientService cliServe = new ClientService();
-		Client c1 = cliServe.createClient();
-//		Client c2 = cliServe.createClient();
-//		
-//		BankAccountService baServe = new BankAccountService();
-//		BankAccount ba1 = baServe.createAccount();
-//		BankAccount ba2 = baServe.createAccount();
 		
-		
+		MethodesMenu mm = new MethodesMenu();
+		mm.start();
+	
 	}
 }
